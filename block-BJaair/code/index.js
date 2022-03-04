1758208251330b55a3828859d4808a8eae202171
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-03-04 13:59:18 +0530

    final push notice-board

---
+++ block-BJaair/code/index.js
@@ -0,0 +1,64 @@
+//let title=document.querySelector('input[name="title"]');
+//let category=document.querySelector('input[name="category"]');
+
+
+let form=document.querySelector('form');
+let ul=document.querySelector('ul');
+let cardsData=JSON.parse(localStorage.getItem('cards')) || [];
+
+
+
+form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let title=event.target.elements.title.value;
+    let category=event.target.elements.category.value;
+    cardsData.push({title,category});
+    localStorage.setItem('cards',JSON.stringify(cardsData));
+    createCardUI(cardsData,ul);
+});
+
+function handleEdit(event,info,id,label){
+    let elm=event.target; //p element
+    let input =document.createElement('input');
+    input.value=info;
+    input.addEventListener('keyup',(e)=>{
+        if(e.keyCode===13){
+            let updatedValue=e.target.value;
+            cardsData[id][label]=updatedValue;
+            createCardUI();
+            localStorage.setItem('cards',JSON.stringify(cardsData))
+        }
+    });
+
+    input.addEventListener('blur',(event)=>{
+        let updatedValue=event.target.value;
+        cardsData[id][label]=updatedValue;
+        createCardUI();
+        localStorage.setItem('cards',JSON.stringify(cardsData));
+    })
+    let parent=event.target.parentElement;
+    parent.replaceChild(input,elm);
+}
+
+function createCardUI(data=cardsData,root=ul){
+    root.innerHTML='';
+    let fragment=new DocumentFragment();
+    data.forEach((cardInfo,index) =>{
+         
+        let li=document.createElement('li');
+        let p=document.createElement('p');
+        p.innerText=cardInfo.category;
+        p.addEventListener('dblclick',(event)=> handleEdit(event,cardInfo.category,index,'category'));
+        let h2=document.createElement('h2');
+        h2.addEventListener('dblclick',(event)=>{
+            handleEdit(event,cardInfo.title,index,'title');
+        })
+        h2.innerText=cardInfo.title;
+        li.append(p,h2);
+        fragment.appendChild(li);
+    });
+    root.append(fragment);
+}
+
+
+createCardUI(cardsData,ul);
